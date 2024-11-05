d5f0c25ef3b508940832f762044c413d171eb73c
Hiba Al Ghouti <114570480+Hibalghouti@users.noreply.github.com>
2024-11-05 17:28:59 +0200

    Update index.py

--- index.py
+++ index.py
@@ -12,8 +12,8 @@ def find_peaks(array):
 array = [1, 3, 2, 5, 4, 6, 3]
 print(find_peaks(array)) 
 
-#for index 1: 3>1 & 3>2 so add 1
+#for index 1: 3>1 & 3>2 so add 1 to the list
 #for index 2: 2!>3 and 2!>5 so dont add 2
-#for index 3: 5>2 and >4 so add 3
+#for index 3: 5>2 and >4 so add 3 to the list
 #for index 4: 4!>5 and !>6 so dont add 4
-#for index 5: 6>4 and 6>3 so add 5
+#for index 5: 6>4 and 6>3 so add 5 to the list
